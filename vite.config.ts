10adf1c067e893ba978a49b1fd532e5400699518
Taspol <taspolsd@gmail.com>
2025-12-25 21:46:31 +0700

    feat: add server config

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => {
       server: {
         port: 3000,
         host: '0.0.0.0',
+        allowedHosts: ['cyber-poc.taspolsd.dev', '.taspolsd.dev'],
       },
       plugins: [react()],
       define: {
